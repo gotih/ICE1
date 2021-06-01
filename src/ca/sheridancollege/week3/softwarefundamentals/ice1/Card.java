2409fbf831101ddcb4970049e66c1bfbf598a57e
harsh goti <Priyanshu Patel@10.0.0.24>
2021-06-01 17:12:01 -0400

    final ICE

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -1,4 +1,6 @@
 /*
+    name : harsh goti
+    Student ID : 991625543
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
